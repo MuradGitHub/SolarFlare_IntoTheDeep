d699df606cf9a57de23c7d1a396802a50f5888e0
MuradGitHub <github@nayal.org>
2025-02-03 09:26:50 -0500

    Create BadInputException.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/error/BadInputException.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.base.error;
+
+public class BadInputException extends RuntimeException {
+    public BadInputException(String messsage) {
+        super(messsage);
+    }
+}
