acdb98db457349ca5e39378367e8d00f55562638
MuradGitHub <github@nayal.org>
2025-02-03 19:37:40 -0500

    Added data provision API to MotorProfiles, to be used in initializing MotorCalibResult

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/MotorProfiles.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/calibration/MotorProfiles.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2025 Murad Nayal
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * (subject to the limitations in the disclaimer below) provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name Murad Nayal nor the names of contributors to this material may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE. THIS
+ * SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+ * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.firstinspires.ftc.teamcode.base.calibration;
 
 import static java.lang.Math.floor;
@@ -6,20 +35,21 @@ import org.firstinspires.ftc.teamcode.base.config.MotorConfig;
 import org.firstinspires.ftc.teamcode.base.config.Validatable;
 import org.firstinspires.ftc.teamcode.base.logging.RobotLogger;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MotorProfiles implements Validatable {
-    private transient final MotorConfig          motorConfig;
+    public  transient final MotorConfig          motorConfig;
     private transient final Logger               logger;
     private           final int                  powerResolution;
-    private           final double               minPower;
-    private           final double               maxPower;
-    private           final double               dP;
-    private                 int                  Pi;
-    private                 int                  Ptarget;
-    private           final MotorProfileConstP[] motorProfilesF;
-    private           final MotorProfileConstP[] motorProfilesR;
+    public            final double               minPower;
+    public           final double               maxPower;
+    private          final double               dP;
+    public                  int                  Pi;
+    public                  int                  Ptarget;
+    public            final MotorProfileConstP[] motorProfilesF;
+    public            final MotorProfileConstP[] motorProfilesR;
 
     public MotorProfiles(MotorConfig motorConfig_in) {
         logger               = RobotLogger.getInstance().getConfigLogger();
@@ -52,6 +82,50 @@ public class MotorProfiles implements Validatable {
         }
     }
 
+    public ArrayList<MotorProfileDataPoint> getDataForward() {
+        ArrayList<MotorProfileDataPoint> data = new ArrayList<>();
+        for(var profile: motorProfilesF) {
+            data.addAll(profile.getProfileData());
+        }
+        return data;
+    }
+
+    public ArrayList<MotorProfileDataPoint> getDataReverse() {
+        ArrayList<MotorProfileDataPoint> data = new ArrayList<>();
+        for (var profile : motorProfilesR) {
+            data.addAll(profile.getProfileData());
+        }
+        return data;
+    }
+
+    public ArrayList<MotorProfileDataPoint> getSteadyStateDataForward() {
+        ArrayList<MotorProfileDataPoint> data = new ArrayList<>();
+        for (var profile : motorProfilesF) {
+            MotorProfileDataPoint ssDataPoint = profile.getSteadyStateResult();
+            if(ssDataPoint == null)
+                continue;
+            data.add(ssDataPoint);
+        }
+        return data;
+    }
+
+    public ArrayList<MotorProfileDataPoint> getSteadyStateDataReverse() {
+        ArrayList<MotorProfileDataPoint> data = new ArrayList<>();
+        for (var profile : motorProfilesR) {
+            MotorProfileDataPoint ssDataPoint = profile.getSteadyStateResult();
+            if(ssDataPoint == null)
+                continue;
+            data.add(ssDataPoint);
+        }
+        return data;
+    }
+
+    public MotorCalibResult getCalibResult() {
+        MotorCalibResult result = new MotorCalibResult(
+
+        );
+        return result;
+    }
     public void writeMetrics() {
         for(int pIdx=0; pIdx<powerResolution; pIdx++) {
             motorProfilesF[pIdx].writeMetrics();
